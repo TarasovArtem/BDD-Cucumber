5396004f6e521ca1631e884b45dda5b9ba13a224
artem1911 <artem.tarasov.11@protonmail.ch>
2023-09-21 16:39:28 +0200

    test: add methods for adding product to cart

---
+++ cypress/e2e/cucumber/Tests/ProductTest/AddProducttotheCart.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps";
+import navigation from "../../Pages/Navigation/Navigation.cy";
+import navigationMenu from "../../Pages/Menu/NavigationMenu.cy";
+import productCard from "../../Pages/ProductCard/ProductCard.cy";
+
+
+Given("I navigate to the Website", () => {
+    navigation.loginPage();
+});
+
+When("I select chosen Product", () => {
+    navigationMenu.clickItemNavigationMenu('Mega Menu');
+    navigationMenu.clickDropDownMenuItem('Headphones')
+    productCard.triggerProductCard('Samsung SyncMaster 941BW')
+});
+
+And('I click the "Add to Cart" button', () => {
+    productCard.clickBtnAddToCard();
+})
